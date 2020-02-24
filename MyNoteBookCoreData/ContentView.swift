d9b5be86416389580dbda378ade57178aed14a67
fredericfilsoof <filsoof95@hotmail.com>
2020-02-24 16:13:15 +0100

    Initial Commit

---
+++ MyNoteBookCoreData/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyNoteBookCoreData
+//
+//  Created by Frederic Filsoof on 24/02/2020.
+//  Copyright © 2020 Frederic Filsoof. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyNoteBookCoreDataTests/MyNoteBookCoreDataTests.swift
@@ -0,0 +1,34 @@
+//
+//  MyNoteBookCoreDataTests.swift
+//  MyNoteBookCoreDataTests
+//
+//  Created by Frederic Filsoof on 24/02/2020.
+//  Copyright © 2020 Frederic Filsoof. All rights reserved.
+//
+
+import XCTest
+@testable import MyNoteBookCoreData
+
+class MyNoteBookCoreDataTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
